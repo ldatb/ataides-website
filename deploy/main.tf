e4a031d838c05cbd5e36116016171d374ac0ac80
Lucas de Ataides <lucasatab@gmail.com>
2025-04-06 21:15:09 -0300

    chore: update s3 policy

--- deploy/main.tf
+++ deploy/main.tf
@@ -18,6 +18,15 @@ resource "aws_s3_bucket_acl" "this" {
     acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "this" {
     bucket = aws_s3_bucket.this.id
 
